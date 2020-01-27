de24f92ec37442bc2a64f6daeaeae91eb9a0f860
Jas Spencer <jas.spencer414@gmail.com>
2020-01-27 19:21:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class App < Sinatra::Base
     @hero_bio = []
     @team_members = params[:team][:members]
     @team_members.each do |hero, attrib|
-      @hero_name << attrib[:name]
-      @hero_power << attrib[:power]
-      @hero_bio << attrib[:bio]
+    @hero_name << attrib[:name]
+    @hero_power << attrib[:power]
+    @hero_bio << attrib[:bio]
     end
 
     erb :team
