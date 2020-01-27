69fb8deaae7de4c4ff591e883adfbe4e704ceab5
Jas Spencer <jas.spencer414@gmail.com>
2020-01-27 19:30:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,9 @@ class App < Sinatra::Base
      @hero_name << hero[:name]
      @hero_power << hero[:power]
      @hero_bio << hero[:bio]
+     erb :team
     end
 
-    erb :team
+    
   end
 end
